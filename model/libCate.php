fe11d19c4236df0ce2c5dc4aee39ad6ac690fb2b
ety001 <ety001@domyself.me>
2013-10-04 17:33:19 +0800

    条目类型中的其他的值改为0

--- model/libCate.php
+++ model/libCate.php
@@ -4,12 +4,12 @@ class libCate extends spModel
     var $pk = "id"; // 数据表的主键
     var $table = "cate"; // 数据表的名称
     var $cateList = array(
+            0 => '其他',
             1 => '三餐',
             2 => '通讯费',
             3 => '穿着打扮',
             4 => '应酬',
             5 => '学习资料',
-            6 => '电子产品',
-            7 => '其他'
+            6 => '电子产品'
         );
 }
